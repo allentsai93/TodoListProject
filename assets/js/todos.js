c046b0579981d97fd6dff9d47f7494fe0ef71cd5
Allen Tsai <allentsai93@gmail.com>
2018-03-07 17:33:48 -0800

    added final touches to styling

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -17,6 +17,16 @@ $("input[type='text']").keypress(function(event){
 	if (event.which === 13){
 		var todoText = $(this).val();
 		$(this).val("");
-		$("ul").append("<li>" + "<span>X</span> " + todoText + "</li>");
+		$("ul").prepend("<li>" + "<span><i class='fas fa-trash'></i></span> " + todoText + "</li>");
 	}
+});
+
+
+$("#toggle-form").click(function(){
+	$("input[type='text']").fadeToggle();
+	if ($("span").children().hasClass("fa-plus")) {
+		$("#toggle-form").html("<i class='fas fa-minus'></i>");
+	} else {
+		$("#toggle-form").html("<i class='fas fa-plus'></i>");
+	};
 });
